a560b81442b632b8c310e01b1c614f66076fe05b
Aigul <sharipova.aig@gmail.com>
2021-04-05 15:56:37 -0400

    react

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,17 +1,25 @@
 // TODO: require yhe skill model
 const Skill = require('../models/skill');
-
-function index(req, res) {
-// TODO: require yhe skill model
+async function index(req, res) {
+   try {
+       const skills = await Skill.find({});
+       res.status(200).json(skills); // send skills as JSON data as an HTTP response
+   } catch (error) {
+       console.log(error);
+       res.status(400).json({ error: 'something went wrong' });
+   }
 }
-
-function create(req, res) {
-// TODO: require yhe skill model
+async function create(req, res) {
+    try {
+        await Skill.create(req.body);
+        res.status(201).json({ msg: 'skill created successfully' });
+    } catch (error) {
+        res.status(401).json({ error: 'something went wrong' });
+    }
 }
-
 module.exports = {
-  index, 
-  create
+    index,
+    create,
 };
 
 
